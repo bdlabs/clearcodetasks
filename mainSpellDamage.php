d67e9758015d7cca6da8d7c24d344d5df22cf2fa
koop <koop@bd.pl>
2017-06-16 09:19:01 +0200

    Fixed of typos

--- mainSpellDamage.php
+++ mainSpellDamage.php
@@ -4,7 +4,7 @@
  */
 class SpellDamage
 {
-    private $spellpatern = array();
+    private $spellpattern = array();
     private $spellsbuffor = array();
     private $damage;
     private $spellstartchars = "fe";
@@ -12,13 +12,13 @@ class SpellDamage
 
     public function __construct()
     {
-        $this->spellpatern["fe"] = 1;
-        $this->spellpatern["je"] = 2;
-        $this->spellpatern["ne"] = 2;
-        $this->spellpatern["ai"] = 2;
-        $this->spellpatern["jee"] = 3;
-        $this->spellpatern["ain"] = 3;
-        $this->spellpatern["dai"] = 5;
+        $this->spellpattern["fe"] = 1;
+        $this->spellpattern["je"] = 2;
+        $this->spellpattern["ne"] = 2;
+        $this->spellpattern["ai"] = 2;
+        $this->spellpattern["jee"] = 3;
+        $this->spellpattern["ain"] = 3;
+        $this->spellpattern["dai"] = 5;
 
     }
 
@@ -63,28 +63,28 @@ class SpellDamage
     {
         $spelltmp = $spell;
 
-        $res1 = substr($spell, 0, 2);
-        $res2 = substr($spell, 0, 3);
-        if (isset($this->spellpatern[$res1])) {
+        $spellsample1 = substr($spell, 0, 2);
+        $spellsample2 = substr($spell, 0, 3);
+        if (isset($this->spellpattern[$spellsample1])) {
             if (strlen(substr($spell, 2)) >= 2) {
-                $this->damage(substr($spell, 2), $string . ',' . $this->spellpatern[$res1]);
+                $this->damage(substr($spell, 2), $string . ',' . $this->spellpattern[$spellsample1]);
             } else {
-                $this->spellsbuffor[] = $string . ',' . $this->spellpatern[$res1];
+                $this->spellsbuffor[] = $string . ',' . $this->spellpattern[$spellsample1];
                 return true;
             }
         }
 
-        if (isset($this->spellpatern[$res2])) {
+        if (isset($this->spellpattern[$spellsample2])) {
             if (strlen(substr($spell, 3)) >= 2) {
-                $this->damage(substr($spell, 3), $string . ',' . $this->spellpatern[$res2]);
+                $this->damage(substr($spell, 3), $string . ',' . $this->spellpattern[$spellsample2]);
             } else {
-                $this->spellsbuffor[] = $string . ',' . $this->spellpatern[$res2];
+                $this->spellsbuffor[] = $string . ',' . $this->spellpattern[$spellsample2];
                 return true;
             }
 
         }
 
-        if (!isset($this->spellpatern[$res1]) && !isset($spellpatern[$res2])) {
+        if (!isset($this->spellpattern[$spellsample1]) && !isset($spellpattern[$spellsample2])) {
             if (strlen($spell) > 0) {
                 $this->damage(substr($spell, 1), $string . ',-1');
             }
